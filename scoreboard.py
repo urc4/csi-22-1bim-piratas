d5bd2e44a050f9bfa9151df6fb8b5b4beacf94f0
danielquintao <danielquintaodemoraes@gmail.com>
2023-04-25 16:52:23 -0300

    refactor scoreboard display code

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,4 @@
-from debug import debug
+import pygame
 
 
 class Scoreboard:
@@ -11,7 +11,12 @@ class Scoreboard:
         self.score = int(self.count / 60)
 
     def display(self):
-        debug(self.score)
+        display_surface = pygame.display.get_surface()
+        font = pygame.font.Font(None, 30)
+        debug_surf = font.render(str(self.score), True, 'White')
+        debug_rect = debug_surf.get_rect(topleft=(10, 10))
+        pygame.draw.rect(display_surface, 'Black', debug_rect)
+        display_surface.blit(debug_surf, debug_rect)
 
     def increase_difficulty(self):
         pass
